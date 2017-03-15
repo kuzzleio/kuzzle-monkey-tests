d4cc93256ecee37ca6df716d9826117ac5cdd2eb
Benoît Vidis <bvidis@kuzzle.io>
2017-03-15 18:43:00 +0100

    wip

--- index.js
+++ index.js
@@ -5,5 +5,16 @@ let suite = new Suite();
 
 return suite.init()
   .then(() => {
-    console.log(suite);
+    EqualFilter = require('./lib/filters/equals');
+    ExistsFIlter = require('./lib/filters/exists');
+    RangeFilter = require('./lib/filters/range');
+
+    const f = new EqualFilter(suite);
+//    console.log(f);
+
+    const f2 = new ExistsFIlter(suite, 'toto');
+    console.log(f2);
+
+    const f3 = new RangeFilter(suite, 'int');
+    console.log(f3);
   });
