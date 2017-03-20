2445f9b63b892932abfc3c2676214ecd466f0e6b
Benoît Vidis <bvidis@kuzzle.io>
2017-03-20 19:03:03 +0100

    wip

--- index.js
+++ index.js
@@ -7,14 +7,23 @@ return suite.init()
   .then(() => {
     console.log(`seed: ${suite.seed}\n\n`);
 
-    const Subscribe = require('./lib/tests/actions/subscribe');
+    const Subscribe = require('./lib/tests/realtime/subscribe');
     const sub = new Subscribe(suite);
     return sub.run();
   })
   .then(() => {
-    const Publish = require('./lib/tests/scenarios/publish');
+    const Publish = require('./lib/tests/realtime/publish');
     const pub = new Publish(suite);
     return pub.run();
+  })
+  .then(() => {
+    const
+      Scheduler = require('./lib/scheduler'),
+      scheduler = new Scheduler(suite);
+
+    scheduler.run();
+
+
   })
   .catch(e => {
     console.error(e);
