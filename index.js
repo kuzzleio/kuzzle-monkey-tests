e8b41de883535f6dfc7eb31dd0eb714a771a48fe
Benoît Vidis <bvidis@kuzzle.io>
2017-03-31 14:40:03 +0200

    randomize execution time

--- index.js
+++ index.js
@@ -7,27 +7,11 @@ return suite.init()
   .then(() => {
     console.log(`seed: ${suite.seed}\n\n`);
 
-    const Subscribe = require('./lib/tests/realtime/subscribe');
-    const sub = new Subscribe(suite);
-    return sub.run();
-  })
-  .then(() => {
-    const Publish = require('./lib/tests/realtime/publish');
-    const pub = new Publish(suite);
-    return pub.run();
-  })
-  .then(() => {
-    const
-      Scheduler = require('./lib/scheduler'),
-      scheduler = new Scheduler(suite);
-
-    scheduler.run();
-
-
+    return suite.scheduler.run();
   })
   .catch(e => {
     console.error(e);
   })
   .finally(() => {
-    console.log(`\n\nseed: ${suite.seed}`);
+    suite.end();
   });
